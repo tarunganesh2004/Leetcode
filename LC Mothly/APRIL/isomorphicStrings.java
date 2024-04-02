bce1bb97dda57baa455127ddefea28a9f54d30c4
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2024-04-02 19:14:35 +0530

    isomorphic Strings

---
+++ LC Mothly/APRIL/isomorphicStrings.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+public class isomorphicStrings {
+    public static void main(String[] args) {
+        String s = "egg";
+        String t = "add";
+        System.out.println(isIsomorphic(s, t));
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            if (m1[s.charAt(i)] != m2[t.charAt(i)])
+                return false;
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+
+    // by using hashmap
+    public static boolean isIsomorphicAnother(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        HashMap<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if (map.containsKey(c1)) {
+                if (map.get(c1) != c2)
+                    return false;
+            } else {
+                if (map.containsValue(c2))
+                    return false;
+                map.put(c1, c2);
+            }
+        }
+        return true;
+    }
+}
