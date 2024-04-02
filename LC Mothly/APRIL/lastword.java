352f342c346f982b3b5deafd903124be165469d0
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2024-04-02 19:07:44 +0530

    length Of last word

--- LC Mothly/APRIL/lastword.java
+++ LC Mothly/APRIL/lastword.java
@@ -1,3 +1,28 @@
 class lastword{
-    
+    public static void main(String[] args) {
+        String s = "Hello World";
+        String s1 = "   fly me to the last moon    ";
+        System.out.println(lengthOfLastWord(s));
+        System.out.println(lengthOfLastWord(s1));
+        System.out.println(anotherway(s));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        String[] words = s.split(" ");
+        if (words.length == 0)
+            return 0;
+        return words[words.length - 1].length();
+    }
+
+    public static int anotherway(String s) {
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ' && length > 0) {
+                return length;
+            } else if (s.charAt(i) != ' ') {
+                length++;
+            }
+        }
+        return length;
+    }
 }
