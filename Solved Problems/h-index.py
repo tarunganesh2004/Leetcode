f1f0b5e5d086a4758498cd8ff35555a734b60365
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-12-06 10:55:08 +0530

    h-index

--- Solved Problems/h-index.py
+++ Solved Problems/h-index.py
@@ -1,3 +1,15 @@
 # H-Index
 
-citations=[]
+citations=[5,1,2,4,1]
+
+def find_Index_Brute(citations):
+    citations.sort(reverse=True)
+    h_index=0
+
+    for i in range(len(citations)):
+        if i+1<=citations[i]:
+            h_index=i+1
+        
+    return h_index
+
+print(find_Index_Brute(citations))
