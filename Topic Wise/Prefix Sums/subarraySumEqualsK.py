7b9a7d72d16bb1f28c63b79fa108a8e442540923
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-12-06 18:07:25 +0530

    lc 560

--- Topic Wise/Prefix Sums/subarraySumEqualsK.py
+++ Topic Wise/Prefix Sums/subarraySumEqualsK.py
@@ -1,6 +1,24 @@
+# LC 560
 nums=[1,1,1,1,1,1]
 t=3
 
 def subarraySum(nums,k):
-    
+    m={0:1}
+    prefixSum=0
+    count=0
 
+    for i in range(len(nums)):
+        prefixSum+=nums[i]
+
+        if prefixSum-k in m:
+            count+=m[prefixSum-k]
+        
+
+        if prefixSum in m:
+            m[prefixSum]+=1
+        else:
+            m[prefixSum]=1
+
+    return count
+
+print(subarraySum(nums,t)) # 2
