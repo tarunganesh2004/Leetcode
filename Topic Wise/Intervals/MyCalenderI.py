95a57faadf0b92beed8772127ade030f3d6fba12
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-12-08 21:04:46 +0530

    optimal

--- Topic Wise/Intervals/MyCalenderI.py
+++ Topic Wise/Intervals/MyCalenderI.py
@@ -1,6 +1,7 @@
 """
 LC 729. My Calendar I
 """
+import bisect
 class MyCalendar:
     def __init__(self):
         self.calendar=[]
@@ -14,7 +15,30 @@ class MyCalendar:
         self.calendar.append((start,end))
         print(self.calendar)
         return True
+
+class MyCalendarIOptimal:
+    def __init__(self):
+        self.calendar=[]
     
+    def book(self,start,end):
+        # Using a balanced BST(Red black tree)
+        # checking if the new interval overlaps with any existing interval
+        # using binary search to find the correct position to insert the new interval
+
+        pos=bisect.bisect_left(self.calendar,(start,end))
+
+        # if there's no overlap with the previous interval
+        if pos>0 and start<self.calendar[pos-1][1]:
+            return False
+        
+        # if there's no overlap with the next interval
+        if pos<len(self.calendar) and end>self.calendar[pos][0]:
+            return False
+        
+        # if no overlap ,insert the new interval
+        self.calendar.insert(pos,(start,end))
+        return True
+
 # input
 cal=MyCalendar()
 print(cal.book(10,20))
