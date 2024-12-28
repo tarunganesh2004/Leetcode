047e15161e949cbbe94f6461672280d65e43b96f
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-12-28 21:06:10 +0530

    permutationsII

--- Topic Wise/BackTracking/permutationsII.py
+++ Topic Wise/BackTracking/permutationsII.py
@@ -2,24 +2,30 @@
 # Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 
 def permuteUnique(nums):
+    # approach is to use a map to keep track of the frequency of each number
+    # then use backtracking to generate the permutations
+    freq_map={}
+    for num in nums:
+        if num in freq_map:
+            freq_map[num]+=1
+        else:
+            freq_map[num]=1
     res=[]
-    s=set()
 
     def backtrack(path):
         if len(path)==len(nums):
-            if tuple(path) not in s:
-                res.append(path)
-                s.add(tuple(path))
+            res.append(path)
             return
-        
-        for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1]:
-                continue
-            if nums[i] not in path:
-                backtrack(path+[nums[i]])
-    nums.sort()
+
+        for num in freq_map:
+            if freq_map[num]>0:
+                freq_map[num]-=1
+                backtrack(path+[num])
+                freq_map[num]+=1 # increase the frequency of the number back to the original value
+
     backtrack([])
     return res
+
 
 nums=[1,1,2]
 print(permuteUnique(nums)) 
