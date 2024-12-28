dd77eaa5c40074ddd43465ecd87bb1033c40078f
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-12-28 21:05:47 +0530

    permutationsII

---
+++ Topic Wise/BackTracking/subsets.py
@@ -0,0 +1,35 @@
+# Subsets LC 78
+
+nums=[1,2,3]
+
+def subsets(nums):
+    res=[]
+    def backtrack(start,path):
+        res.append(path)
+        for i in range(start,len(nums)):
+            backtrack(i+1,path+[nums[i]])
+    backtrack(0,[])
+    return res
+
+def anotherApproach(nums):
+    res=[]
+
+    subset=[]
+    def generate(i):
+        if i>=len(nums):
+            res.append(subset.copy())
+            return
+        
+        # include
+        subset.append(nums[i])
+        generate(i+1)
+
+        # not include
+        subset.pop()
+        generate(i+1)
+    
+    generate(0)
+    return res
+
+print(subsets(nums))
+print(anotherApproach(nums))
