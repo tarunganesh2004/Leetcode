750d153847b7e641223178f3ec7bfcb8d71b0574
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-12-29 19:09:31 +0530

    word Search II

---
+++ Topic Wise/BackTracking/wordSearchII.py
@@ -0,0 +1,8 @@
+# LC 212 Word Search II
+# Given an m x n board of characters and a list of strings words, return all words on the board.
+
+# Extension of the previous problem wordSearch.py
+# bruteforce works but is not efficient
+
+# The idea is to use a trie to store the words and then use dfs to search for the words in the board
+
