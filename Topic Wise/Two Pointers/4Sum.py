3b557f06b7ba9001f9d2c01aef49b3d2dace1b44
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2025-01-20 21:23:51 +0530

    Add solution for 4 Sum (LC 18) with brute force, hashing, and optimized approaches

--- Topic Wise/Two Pointers/4Sum.py
+++ Topic Wise/Two Pointers/4Sum.py
@@ -1,40 +1,77 @@
-def _4Sum(nums,target):
-    nums.sort()
-    res=set()
-    n=len(nums)
-
-    for i in range(n-3):
-            
-            if i>0 and nums[i]==nums[i-1]:
-                continue
-    
-            for j in range(i+1,n-2):
-    
-                if j>i+1 and nums[j]==nums[j-1]:
-                    continue
-    
-                l,r=j+1,n-1  # noqa: E741
-
-                while l<r:
-                    t=nums[i]+nums[j]+nums[l]+nums[r]
-    
-                    if t<target:
-                        l+=1
-                    elif t>target:
-                        r-=1
-                    else:
-                        res.add((nums[i],nums[j],nums[l],nums[r]))
-    
-                        while l<r and nums[l]==nums[l+1]:
-                            l+=1
-                        while l<r and nums[r]==nums[r-1]:
-                            r-=1
-                            
-                        l+=1  # noqa: E741
-                        r-=1
-    
-    return res
+# 4 Sum LC 18
 
 a=[1, 0, -1, 0, -2, 2]
 target=0
-print(_4Sum(list(a),target)) # {(-2, -1, 1, 2), (-2, 0, 0, 2), (-1, 0, 0, 1)}
+
+# brute force approach is to use 4 nested loops 
+def bruteforce(arr,target): # O(n^5) time complexity
+    res=[]
+    n=len(arr)
+    for i in range(n):
+        for j in range(i+1,n):
+            for k in range(j+1,n):
+                for l in range(k+1,n):  # noqa: E741
+                    if arr[i]+arr[j]+arr[k]+arr[l]==target:
+                        cur=[arr[i],arr[j],arr[k],arr[l]]
+                        cur.sort()
+                        if cur not in res:
+                            res.append(cur)
+    return res
+
+# Better approach is to use a hashmap to store the sum of 2 elements and then check if the negative of the sum of the other 2 elements is in the hashmap
+def fourSumUsingHashing(arr,target):
+    res_set=set() # O(n^3) time complexity & O(n) space complexity
+    n=len(arr)
+
+    for i in range(n):
+        for j in range(i+1,n):
+            s=set()
+            for k in range(j+1,n):
+                sum_val=arr[i]+arr[j]+arr[k]
+                last=target-sum_val
+                if last in s:
+                    cur=sorted([arr[i],arr[j],arr[k],last])
+                    res_set.add(tuple(cur))
+                s.add(arr[k])
+    return [list(x) for x in res_set]
+
+def fourSumOptimized(arr,target):
+    n=len(arr) # O(n^3) time complexity & O(1) space complexity
+    arr.sort()
+    res=[]
+    for i in range(n):
+        # skip duplicates for i
+        if i>0 and arr[i]==arr[i-1]:
+            continue
+        for j in range(i+1,n):
+            # skip duplicates for j
+            if j>i+1 and arr[j]==arr[j-1]:
+                continue
+            left=j+1
+            right=n-1
+            # Two pointer approach
+            while left<right:
+                total=arr[i]+arr[j]+arr[left]+arr[right]
+                if total==target:
+                    res.append([arr[i],arr[j],arr[left],arr[right]])
+                    # skip duplicates for left
+                    while left<right and arr[left]==arr[left+1]:
+                        left+=1
+                    # skip duplicates for right
+                    while left<right and arr[right]==arr[right-1]:
+                        right-=1
+                    left+=1
+                    right-=1
+                
+                # if total is less than target, increment left
+                elif total<target:
+                    left+=1
+                # if total is greater than target, decrement right
+                else:
+                    right-=1
+    return res
+
+
+print(bruteforce(a,target)) # [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
+print(fourSumUsingHashing(a,target)) # [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
+print(fourSumOptimized(a,target)) # [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
