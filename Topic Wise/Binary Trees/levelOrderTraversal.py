831c485a733fecb6a2593d91bfb0bea46b49c6fc
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2025-02-04 21:51:57 +0530

    Add solution for Level Order Traversal of Binary Tree (LC 102)

---
+++ Topic Wise/Binary Trees/levelOrderTraversal.py
@@ -0,0 +1,54 @@
+# Level Order Traversal LC 102
+
+class TreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def build_tree(self,arr):
+        if not arr or arr[0] == 'N':
+            return None
+        
+        root = TreeNode(arr[0])
+        i = 1
+        q = []
+        q.append(root)
+        while i < len(arr) and q:
+            node = q.pop(0)
+            if arr[i] != 'N':
+                node.left = TreeNode(arr[i])
+                q.append(node.left)
+            i += 1
+            if i>=len(arr):
+                break
+            if i < len(arr) and arr[i] != 'N':
+                node.right = TreeNode(arr[i])
+                q.append(node.right)
+            i += 1
+
+        return root
+    
+    def level_order_traversal(self,root):
+        if not root:
+            return []
+        q = []
+        q.append(root)
+        res = []
+        while q:
+            level = []
+            for i in range(len(q)):
+                node = q.pop(0)
+                level.append(node.data)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(level)
+        return res
+    
+
+arr=[1,2,3,4,5,6,7]
+root=TreeNode(arr[0])
+root=root.build_tree(arr)
+print(root.level_order_traversal(root)) # type: ignore
