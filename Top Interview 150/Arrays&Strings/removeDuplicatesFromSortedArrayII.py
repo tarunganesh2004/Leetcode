90d1a4eaa1652ee0527924c9b6f5e94f4ba471b9
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2025-02-06 16:58:42 +0530

    Add alternative solution for Remove Duplicates from Sorted Array II (LC 80) using a simplified approach

--- Top Interview 150/Arrays&Strings/removeDuplicatesFromSortedArrayII.py
+++ Top Interview 150/Arrays&Strings/removeDuplicatesFromSortedArrayII.py
@@ -15,4 +15,13 @@ def removeDuplicates(nums):
             j+=1
     return j
 
+# simple way
+def remove(nums):
+    i=2
+    for j in range(2,len(nums)):
+        if nums[j]>nums[i-2]:
+            nums[i]=nums[j]
+            i+=1
+    return i
+
 print(removeDuplicates(nums))
