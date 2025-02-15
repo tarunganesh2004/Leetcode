507d8b9232cb47af9bf05e5bbd858d604f106929
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-02-15 08:39:32 +0530

    Refactor optimizedWay function to include a comment for precomputed variable

--- LC Mothly/2025/February/15th_feb.py
+++ LC Mothly/2025/February/15th_feb.py
@@ -27,7 +27,7 @@ def punishmentNumber(n): # O(n^2)
 
 
 def optimizedWay(n):
-    ans=0
+    ans=0 # precomputed
     l = [0, 1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657, 675, 703, 756, 792, 909, 918, 945, 964, 990, 991, 999, 1000]  # noqa: E741
     for i in l:
         if i>n:
