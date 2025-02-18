c8be90b6beda88d6879586e8442dcda9934c22f9
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-02-18 18:43:18 +0530

    Add solution for Construct Smallest Number from DI String (LC 2375) with stack-based approach

---
+++ LC Mothly/2025/February/18th_feb.py
@@ -0,0 +1,29 @@
+# # Construct Smallest Number from DI String LC 2375
+
+# You are given a 0-indexed string pattern of length n consisting of the characters 'I' meaning increasing and 'D' meaning decreasing.
+
+# A 0-indexed string num of length n + 1 is created using the following conditions:
+
+# num consists of the digits '1' to '9', where each digit is used at most once.
+# If pattern[i] == 'I', then num[i] < num[i + 1].
+# If pattern[i] == 'D', then num[i] > num[i + 1].
+# Return the lexicographically smallest possible string num that meets the conditions.
+
+pattern="IIIDIDDD"
+
+def smallestNumber(pattern):
+    st=[]
+    res=[]
+    num=1
+    for i in range(len(pattern)+1):
+        st.append(num)
+        num+=1
+
+        if i==len(pattern) or pattern[i]=='I':
+            while st:
+                res.append(str(st.pop()))
+    return "".join(res)
+
+
+
+print(smallestNumber(pattern))
