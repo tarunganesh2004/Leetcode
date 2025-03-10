deb2b10a8e2268da16a84a18298db31ca152d4f1
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-10 10:16:51 +0530

    Add solution for Count of Substrings Containing Every Vowel and K Consonants II (LC 3306)

---
+++ LC Mothly/2025/March/10th_march.py
@@ -0,0 +1,32 @@
+# Count of Substrings Containing Every Vowel and K Consonants II LC 3306
+
+s="aeioqq"
+k=1
+
+def bruteForce(s,k):
+    n=len(s)
+    def solve(str,k):
+        v="aeiou"
+        arr=[0]*5
+        cons=0
+        for ch in str:
+            for i in range(5):
+                if v[i]==ch:
+                    arr[i]+=1
+        
+        for ch in str:
+            if ch not in v:
+                cons+=1
+
+        all_v=(sum(arr)==5)
+        all_c=(cons==k)
+        return all_v and all_c
+    res=0
+    for i in range(n):
+        for j in range(i,n):
+            str=s[i:j+1]
+            if solve(str,k):
+                res+=1
+    return res
+
+print(bruteForce(s,k))
