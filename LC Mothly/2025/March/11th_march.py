352325a6ff9bb668bf74b138a81529e824e9fe9f
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-11 22:21:48 +0530

    Add brute force solution for Number of Substrings Containing All Three Characters (LC 1358)

---
+++ LC Mothly/2025/March/11th_march.py
@@ -0,0 +1,14 @@
+# Number of Substrings Containing All Three Characters LC 1358
+
+s="abcabc"
+
+# brute force
+def bruteForce(s):
+    n=len(s)
+    res=0
+    for i in range(n):
+        for j in range(i,n):
+            if len(set(s[i:j+1]))==3:
+                res+=1
+    return res
+
