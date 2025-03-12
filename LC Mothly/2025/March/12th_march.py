3adb591af12382269695319976c1c7ea7a1c61aa
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-12 09:34:33 +0530

    Add optimized binary search approach for counting positive and negative integers

--- LC Mothly/2025/March/12th_march.py
+++ LC Mothly/2025/March/12th_march.py
@@ -14,3 +14,37 @@ def maxCount(nums):
     return max(pos,neg)
 
 print(maxCount(nums))
+
+# optimized approach is to use binary search
+def optimized(nums):
+    # return 1st index where value >=0
+    def lower_bound(nums):
+        start,end=0,len(nums)-1
+        n=len(nums)
+        while start<=end:
+            mid=(start+end)//2
+            if nums[mid]<0:
+                start=mid+1
+            else:
+                end=mid-1
+                n=mid
+        return n
+    
+    # return 1st index where value >0
+    def upper_bound(nums):
+        start,end=0,len(nums)-1
+        n=len(nums)
+        while start<=end:
+            mid=(start+end)//2
+            if nums[mid]<=0:
+                start=mid+1
+            else:
+                end=mid-1
+                n=mid
+        return n
+    
+    positive_count=len(nums)-upper_bound(nums)
+    negative_count=lower_bound(nums)
+    return max(positive_count,negative_count)
+
+print(optimized(nums))
