89bd991bb7beb91cb08e9210c2217ed5b4fb39e6
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-14 15:56:25 +0530

    Refactor maxCandies function for clarity and efficiency in binary search implementation

--- LC Mothly/2025/March/14th_march.py
+++ LC Mothly/2025/March/14th_march.py
@@ -4,25 +4,24 @@ candies=[5,8,6]
 k=3
 
 def maxCandies(candies,k):
-    if sum(candies)<k:
-        return 0
-    n=len(candies)
-    left=1
-    right=max(candies)
-    ans=0
-    while left<=right:
-        numOfPiles=0
-        mid=(left)+(right-left)//2
+    n = len(candies)
+    left = 1  # the least number of candy in each stack we can give to each student is one
+    right = max(candies)  # the max number of candy in each stack that we can give to each student is the maximum number in the candies array
+    ans = 0 # ans here is used to store the maximum amount in each stack that we can give to each children. 
+               # If we don't have enough to distribute, we will return 0 at the end so we initialize it to be 0 now.
 
-        for i in range(n):
-            numOfPiles+=candies[i]//mid
+    while left <= right:  # binary search
+        numberOfPiles = 0
+        mid = (left) + (right - left) // 2  # the number of candies we require to form a stack
 
-        if numOfPiles>=k:
-            ans=max(ans,mid)
-            left=mid+1
-        else:
-            right=mid-1
+        for i in range(n):   # loop through the array to find the numbers of stack we can form
+            numberOfPiles += candies[i] // mid   # we add to the numberOfPiles whenever we find that this current stack (candies[i]) can be split into mid (the number of candies we require to form a stack)
 
+        if numberOfPiles >= k: # if our number of piles is greater or equal than the students we have, so we have enough to distribute
+            ans = max(ans, mid)   # we first store the max no. of candies in each stack that we can give to each student 
+            left = mid + 1      # we will try to increase the number of candies in each stack that we can give to each student
+        else: 
+            right = mid - 1   # we will try to reduce the number of candies in each stack that we can give to each student
     return ans
 
 # optimized way(another solution)
