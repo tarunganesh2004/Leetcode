63c3994ce8ed22ebd16ce97672b7f01f43d0d1b2
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-18 10:54:46 +0530

    Add implementation for Longest Nice Subarray (LC 2401) using brute force approach

---
+++ LC Mothly/2025/March/18th_march.py
@@ -0,0 +1,26 @@
+# # Longest Nice Subarray LC 2401
+# You are given an array nums consisting of positive integers.
+
+# We call a subarray of nums nice if the bitwise AND of every pair of elements that are in different positions in the subarray is equal to 0.
+
+# Return the length of the longest nice subarray.
+
+# A subarray is a contiguous part of an array.
+
+# Note that subarrays of length 1 are always considered nice.
+
+ 
+
+nums=[1,3,8,48,10]
+
+def longestNiceBruteForce(nums):
+    n=len(nums)
+    for i in range(n,0,-1):
+        for j in range(n-i+1):
+            sub=nums[j:j+i]
+            print(sub)
+            if all(sub[k]&sub[l]==0 for k in range(len(sub)) for l in range(k+1,len(sub))):
+                return i
+    return 0
+
+print(longestNiceBruteForce(nums))
