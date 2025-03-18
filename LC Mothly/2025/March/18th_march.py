69372aa8ea8c302b8da12d9425700a66a3f24eef
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-18 10:57:33 +0530

    Add sliding window implementation for Longest Nice Subarray

--- LC Mothly/2025/March/18th_march.py
+++ LC Mothly/2025/March/18th_march.py
@@ -23,4 +23,19 @@ def longestNiceBruteForce(nums):
                 return i
     return 0
 
+# sliding window approach
+def longestNiceSubarray(nums):
+    used=0
+    j=0
+    res=0
+    for i in range(len(nums)):
+        while((used&nums[i])!=0):
+            used^=nums[j]
+            j+=1
+        used|=nums[i]
+        res=max(res,i-j+1)
+    return res
+
+
 print(longestNiceBruteForce(nums))
+print(longestNiceSubarray(nums))
