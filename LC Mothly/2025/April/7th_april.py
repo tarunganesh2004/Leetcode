a7ffc663171c1559b0140a0e4f70e41299ffe397
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-04-08 11:12:11 +0530

    Add memoization implementation for Partition Equal Subset Sum (LC 416)

--- LC Mothly/2025/April/7th_april.py
+++ LC Mothly/2025/April/7th_april.py
@@ -12,4 +12,16 @@ def brute_force(nums):
     
     return can_partition(0, 0, 0)
 
+# using lru_cache for memoization
+from functools import lru_cache  # noqa: E402
+def memoization(nums):
+    @lru_cache(None)
+    def can_partition(i, sum1, sum2):
+        if i == len(nums):
+            return sum1 == sum2
+        return can_partition(i + 1, sum1 + nums[i], sum2) or can_partition(i + 1, sum1, sum2 + nums[i])
+    
+    return can_partition(0, 0, 0)
+
 print(brute_force(nums))
+print(memoization(nums))
