a26818a0f2533f8b64e94dc93c47cb3407806b6c
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-04-08 11:11:42 +0530

    Add implementation for Partition Equal Subset Sum (LC 416)

---
+++ LC Mothly/2025/April/7th_april.py
@@ -0,0 +1,15 @@
+# Partition Equal Subset Sum LC 416
+
+nums=[1,5,11,5]
+
+# brute force
+def brute_force(nums):
+    # recursion
+    def can_partition(i, sum1, sum2):
+        if i == len(nums):
+            return sum1 == sum2
+        return can_partition(i + 1, sum1 + nums[i], sum2) or can_partition(i + 1, sum1, sum2 + nums[i])
+    
+    return can_partition(0, 0, 0)
+
+print(brute_force(nums))
