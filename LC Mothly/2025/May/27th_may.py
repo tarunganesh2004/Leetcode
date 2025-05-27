095d0de8d9e04043064b0c202cd75105a5f2ee37
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-05-27 11:47:01 +0530

    Add optimized function for calculating the difference of sums

--- LC Mothly/2025/May/27th_may.py
+++ LC Mothly/2025/May/27th_may.py
@@ -13,4 +13,15 @@ def differenceOfSums(n,m):
             s1 += i
     return s1-s2
 
+# optimized
+def optimized(n,m):
+    # not divisible - divisible by m 
+    # not divisble=total- divisble by m 
+    total = n * (n + 1) // 2
+    #  from 1 to n, n//m is the count of numbers divisible by m
+    k= n // m
+    divisible = m * (k * (k + 1)) // 2
+    return total-2*divisible
+
 print(differenceOfSums(n,m))
+print(optimized(n,m))
