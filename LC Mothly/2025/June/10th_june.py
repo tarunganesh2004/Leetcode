5c9e76f255a177a4abcd356c9e3b9c153ef69bbd
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-06-10 11:26:03 +0530

    Add implementation for maximum difference between even and odd frequency (LC 3442)

---
+++ LC Mothly/2025/June/10th_june.py
@@ -0,0 +1,13 @@
+# Maximum Difference Between Even and Odd Frequency I  LC 3442
+
+s="aaaaabbc"
+
+def maxDifference(s):
+    from collections import Counter
+
+    freq= Counter(s)
+    maxOdd=max(v for v in freq.values() if v % 2 == 1)
+    maxEven=max(v for v in freq.values() if v % 2 == 0)
+    return maxOdd - maxEven
+
+print(maxDifference(s))  # Output: 3
